d8a50528312a465b89c8da90e53d8c1e545c5e77
quanlh_560 <quanlh_560@vnu.edu.vn>
2014-09-25 22:29:43 +0700

    firt commit

---
+++ Triangle.java
@@ -0,0 +1,28 @@
+package main;
+
+public class Triangle {
+	private int a1, a2, a3;
+
+	public Triangle(int a1, int a2, int a3) {
+		this.a1 = a1;
+		this.a2 = a2;
+		this.a3 = a3;
+	}
+
+	public void checkTriangle() {
+		if (a1 <=0 || a2 <=0 || a3 <=0)
+			System.out.println("NotAtriangle");
+		else if (a1 + a2 <= a3 || a1 + a3 <= a2 || a2 + a3 <= a1)
+			System.out.println("NotATriangle");
+		else if (a1 == a2 || a1 == a3 || a2 == a3)
+			System.out.println("Isosceles");
+		else if (a1 == a2 && a2 == a3)
+			System.out.println("Equilateral");
+		else if (a1 * a1 + a2 * a2 == a3 * a3 || a1 * a1 + a3 * a3 == a2
+				|| a2 * a2 + a3 * a3 == a1 * a1)
+			System.out.println("Right Triangle");
+		else
+			System.out.println("Scalene");
+	}
+
+}
---
+++ testTriangle.java
@@ -0,0 +1,9 @@
+package main;
+
+public class testTriangle {
+	public static void main(String[] args) {
+		Triangle tr = new Triangle(3, 4, 5);
+		tr.checkTriangle();
+	}
+
+}
