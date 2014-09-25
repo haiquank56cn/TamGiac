175d69b8033b76e5ddc445380ccbae8ec8fc3a13
lê hải quân <haiquank56cn@gmail.com>
2014-09-25 22:44:10 +0700

    Update testTriangleTest.java

--- testTriangleTest.java
+++ testTriangleTest.java
@@ -7,8 +7,16 @@ import org.junit.Test;
 public class testTriangleTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void tamGiacvuong() {
+		Triangle T= new Triangle();
+		Assert.assertEquals("Right Triangle",T.Triangel(3,4,5));
 	}
+	public void tamGiacCan() {
+		Triangle T= new Triangle();
+		Assert.assertEquals("Isosceles",T.Triangel(4,3,3));
+	}
+	public void not() {
+		Triangel T= new Triangle();
+		Assert.assertEquals("NotATriangle",T.Triangel(4,2,2));
 
 }
